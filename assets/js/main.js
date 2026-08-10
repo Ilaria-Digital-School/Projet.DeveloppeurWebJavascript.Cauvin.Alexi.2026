922d7bd2f384dc27f1c024b7c6b6d12a6da8aad3
AlexiCauvin <alexi.cauvin@gmail.com>
2026-08-10 16:22:10 +0200

    DevFront Ajout page connexion / inscription

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,37 @@
-function afficherTagFilter(){
+function afficherTagFilter() {
 
     let bouton = document.querySelector("#afficherFiltreTag");
     let wrapperTag = document.querySelector(".wrapper_tag");
 
+    if (!bouton || !wrapperTag) {
+        return;
+    }
+
+
     bouton.addEventListener("click", () => {
         wrapperTag.classList.toggle("hidden");
     });
-
-    
 }
 
 afficherTagFilter();
+
+
+function connexion() {
+    const showSignIn = document.getElementById('showSignIn');
+    const showSignUp = document.getElementById('showSignUp');
+    const wrapper = document.getElementById('wrapper_connexion');
+
+    if(!showSignIn || !showSignUp || !wrapper){
+        return;
+    }
+
+    showSignUp.addEventListener('click', () => {
+        wrapper.classList.add('right_panel_active');
+    });
+
+    showSignIn.addEventListener('click', () => {
+        wrapper.classList.remove('right_panel_active');
+    });
+}
+
+connexion();
